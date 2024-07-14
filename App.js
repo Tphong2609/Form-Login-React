6dffeb2ed350995ed9385cb729371cf89e797118
Tphong2609 <phongtuanvo13@gmail.com>
2024-07-14 19:00:50 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+// import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+// import HomePage from "./components/pages/HomePage";
+import Demo from "./components/Demo";
+export default function App() {
+  //code js here:
+
+  return <Demo />;
+}
